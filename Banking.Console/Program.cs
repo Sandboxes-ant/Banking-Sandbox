93230cbebe6993bda7e1b0af01250c94bb095e36
Kasper Thrysøe <kasper@thrysoe.dk>
2022-08-30 16:57:30 +0200

    First few lines

--- Banking.Console/Program.cs
+++ Banking.Console/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int AccountId = 0;
+int AccountBalance = 0;
+
+Random random = new Random();
+AccountBalance = random.Next(1,50000);
+
+//Welcome and account info
+Console.WriteLine("Welcome to Thrys Banking");
+Console.WriteLine("Your account number is: " + AccountId);
+Console.WriteLine("Your current balance is: " + AccountBalance);
+Console.ReadKey();
+
+
+Console.WriteLine("Please select an option");
+Console.WriteLine("1: Insert money");
+Console.WriteLine("2: Withdrawal money");
+
+//Check input
