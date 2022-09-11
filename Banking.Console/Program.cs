2fa64bd0d4b27f2085247af628d55bacee01cec5
Kasper Thrysøe <kasper@thrysoe.dk>
2022-09-11 11:44:18 +0200

    Adjusted accountId to be an always non-negative variable Adjusted variable naming Removed random accountId generation

--- Banking.Console/Program.cs
+++ Banking.Console/Program.cs
@@ -1,18 +1,16 @@
-﻿int AccountId = 0;
-int AccountBalance = 0;
-int Input = 0;
-int AmountOfMoney = 0;
+﻿uint accountId = 25;
+int accountBalance = 0;
+int input = 0;
+int amountOfMoney = 0;
 
 Random random = new Random();
-AccountBalance = random.Next(1, 50000);
+accountBalance = random.Next(1, 50000);
 
-Random random2 = new Random();
-AccountId = random2.Next(1, 10);
 
 //Welcome and account info
 Console.WriteLine("Welcome to Thrys Banking");
-Console.WriteLine("Your account number is: " + AccountId);
-Console.WriteLine("Your current balance is: " + AccountBalance);
+Console.WriteLine("Your account number is: " + accountId);
+Console.WriteLine("Your current balance is: " + accountBalance);
 Console.WriteLine("Please press any buttom to continue");
 Console.ReadKey();
 
@@ -20,29 +18,29 @@ Console.WriteLine("Please select an option");
 Console.WriteLine("1: Insert money");
 Console.WriteLine("2: Withdrawal money");
 
-Input = Convert.ToInt32(Console.ReadLine());
+input = Convert.ToInt32(Console.ReadLine());
 
-if (Input == 1)
+if (input == 1)
 {
     Console.WriteLine("You have selected to insert money");
     Console.WriteLine("How much money would you like to insert?");
-    AmountOfMoney = Convert.ToInt32(Console.ReadLine());
-    AccountBalance = AccountBalance + AmountOfMoney;
-    Console.WriteLine("Your total balance is now: " + AccountBalance);
+    amountOfMoney = Convert.ToInt32(Console.ReadLine());
+    accountBalance = accountBalance + amountOfMoney;
+    Console.WriteLine("Your total balance is now: " + accountBalance);
     Console.WriteLine("Bye!");
     Console.ReadKey();
     Environment.Exit(0);
 }
 
-if (Input == 2)
+else if (input == 2)
 {
     Console.WriteLine("You have selected to withdraw money");
     Console.WriteLine("How much money would you like to withdraw?");
-    Console.WriteLine("Your balance is: " + AccountBalance);
-    AmountOfMoney = Convert.ToInt32(Console.ReadLine());
-    AccountBalance = AccountBalance - AmountOfMoney;
+    Console.WriteLine("Your balance is: " + accountBalance);
+    amountOfMoney = Convert.ToInt32(Console.ReadLine());
+    accountBalance = accountBalance - amountOfMoney;
 
-    if (AccountBalance <= 0)
+    if (accountBalance <= 0)
     {
         Console.WriteLine("You cannot withdraw more money than you own!");
         Console.WriteLine("Bye!");
@@ -52,8 +50,8 @@ if (Input == 2)
 
     else
     {
-        Console.WriteLine("You have withdrawed: " + AmountOfMoney);
-        Console.WriteLine("Your new balance is: " + AccountBalance);
+        Console.WriteLine("You have withdrawed: " + amountOfMoney);
+        Console.WriteLine("Your new balance is: " + accountBalance);
         Console.WriteLine("Bye!");
         Console.ReadKey();
         Environment.Exit(0);
