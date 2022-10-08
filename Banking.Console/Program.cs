d30b863cf30e66723257d9c7760a3b633c465b15
Thrys1337 <112279076+Thrys1337@users.noreply.github.com>
2022-10-08 09:53:22 +0200

    Dev iteration1 (#2)
    * First few lines
    
    * initial release
    
    * Adjusted accountId to be an always non-negative variable
    Adjusted variable naming
    Removed random accountId generation
    
    * Added changes
    switch case instead of if statements

--- Banking.Console/Program.cs
+++ Banking.Console/Program.cs
@@ -1,2 +1,71 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿uint accountId = 25;
+int accountBalance = 0;
+int input = 0;
+int amountOfMoney = 0;
+
+Random random = new Random();
+accountBalance = random.Next(1, 50000);
+
+
+//Welcome and account info
+Console.WriteLine("Welcome to Thrys Banking");
+Console.WriteLine("Your account number is: " + accountId);
+Console.WriteLine("Your current balance is: " + accountBalance);
+Console.WriteLine("Please press any buttom to continue");
+Console.ReadKey();
+
+Console.WriteLine("Please select an option");
+Console.WriteLine("1: Insert money");
+Console.WriteLine("2: Withdrawal money");
+
+input = Convert.ToInt32(Console.ReadLine());
+
+switch (input)
+{
+    case < 0:
+        Environment.Exit(0);
+        break;
+    
+    case 1:
+        Console.WriteLine("You have selected to insert money");
+        Console.WriteLine("How much money would you like to insert?");
+        amountOfMoney = Convert.ToInt32(Console.ReadLine());
+        accountBalance = accountBalance + amountOfMoney;
+        Console.WriteLine("Your total balance is now: " + accountBalance);
+        Console.WriteLine("Bye!");
+        Console.ReadKey();
+        Environment.Exit(0);
+        break;
+    
+    case 2:
+        Console.WriteLine("You have selected to withdraw money");
+        Console.WriteLine("How much money would you like to withdraw?");
+        Console.WriteLine("Your balance is: " + accountBalance);
+        amountOfMoney = Convert.ToInt32(Console.ReadLine());
+        accountBalance = accountBalance - amountOfMoney;
+
+        if (accountBalance <= 0)
+        {
+            Console.WriteLine("You cannot withdraw more money than you own!");
+            Console.WriteLine("Bye!");
+            Console.ReadKey();
+            Environment.Exit(0);
+        }
+
+        else
+        {
+            Console.WriteLine("You have withdrawed: " + amountOfMoney);
+            Console.WriteLine("Your new balance is: " + accountBalance);
+            Console.WriteLine("Bye!");
+            Console.ReadKey();
+            Environment.Exit(0);
+
+        }
+        break;
+    
+    case > 2:
+        Console.Write("Error :-) Function not added yet!");
+        Console.ReadKey();
+        Environment.Exit(0);
+        break;
+}
